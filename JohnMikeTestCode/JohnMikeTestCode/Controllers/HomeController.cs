4fda01e2bae198da4eb4d69f4df544dce82b390a
jmdecastra <jmdecastra@crimson.ua.edu>
2013-10-14 15:46:49 -0500

    test 2

--- JohnMikeTestCode/JohnMikeTestCode/Controllers/HomeController.cs
+++ JohnMikeTestCode/JohnMikeTestCode/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 //this is a test of our weekly tornado warning.
+//test 2
 namespace JohnMikeTestCode.Controllers
 {
     public class HomeController : Controller
